85f0320b78af71b1c730ffc348340b288de3268a
Samer  El-Murr <snjmurr@gmail.com>
2022-11-08 14:04:04 +0200

    This is the full mini demo app for amplify that has a simple lambda function

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "customerId": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "basicSetupEx": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
